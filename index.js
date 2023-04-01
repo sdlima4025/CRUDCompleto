6f18b0930c914629bd6eaf60418943776b11f328
Sérgio Lima <sergiolimafg3@gmail.com>
2023-03-31 21:16:02 -0300

    desenvolvida a lógica do botão deletar

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 var dados = []
- 
+ // DELETE
+ function ApagaRegistro(id) {
+  let _confirm = confirm("Deseja Realmente excluir este registro?")
+
+  if(_confirm) {
+    for(let i = 0; i < dados.length; i++) {
+      if (dados[i].ID == id) {
+        // A função splice apaga um elemento do array
+          dados.splice(i, 1)
+      }
+    }
+
+    PopulaTabela()
+  }
+ }
+
+ //EDITAR
+ function EditaRegistro(id) {
+  
+ }
+
+
 function PopulaTabela() {
   if(Array.isArray(dados)) {
+
+  localStorage.setItem("__dados__", JSON.stringify(dados))
 
     //Limpando atabela 
     $("#tblDados tbody").html("")
@@ -11,16 +34,18 @@ dados.forEach(function (item) {
   $("#tblDados tbody").append(
     `<tr>
       <td>${item.ID}</td>
-      <td>${item.nome}</td>
+      <td>${item.Nome}</td>
       <td>${item.Sobrenome}</td>
       <td>${item.DtNascimento}</td>
       <td>${item.Formacao}</td>
+      <td><button type="button" class="btn btn-primary"><i class="fa fa-edit"></i></button></td>
+      <td><button type="button" class="btn btn-danger" onclick="javascript:ApagaRegistro(${item.ID});"><i class="fa fa-trash"</button></td>
     </tr>`)
     })
   }
 }
 
-
+// CREAT
 // Bloco JQuery
 $(function () {
   // EXECUTA AO CARREGAR DA TELA 
@@ -35,15 +60,37 @@ $(function () {
 
     let Nome = $("#txtNome").val()
     let Sobrenome = $("#txtSobrenome").val()
-    let DtNascimento = $("#txtDtNascimento").val()
+    let DtNascimento = new  Date($("#txtDtNascimento").val()).toLocaleDateString("pt-br", {timeZone: "UTC"})
     let Formacao = $("#txtFormacao").val()
 
 
 
-    let registro{}
+    let registro = {}
+
+    registro.Nome = Nome
+    registro.Sobrenome = Sobrenome
+    registro.DtNascimento = DtNascimento
+    registro.Formacao = Formacao
+
+    registro.ID = dados.length + 1
+
+    // Função push adiciona um elemento ao array
+    dados.push(registro)
+
+    alert("Registro salvo com sucesso!")
+    $("#modalRegistro").modal("hide")
+
+
+    // Limpando os inputs
+    $("#txtNome").val("")
+    $("#txtSobrenome").val("")
+    $("#txtDtNascimento").val("")
+    $("#txtFormacao").val("")
+
+    PopulaTabela()
   })
 })
-// CREAT
+
 // READ
 // UPDATE
-// DELETE
+
